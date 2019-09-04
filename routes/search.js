8c0b51d61a5496605b5b550ce7f1964aa39dd754
Vot <vot@siliconfen.co>
2019-09-04 18:39:24 +0100

    Add basic templates

--- routes/search.js
+++ routes/search.js
@@ -1,3 +1,10 @@
+const _ = require('lodash');
+
 module.exports = (req, res, next) => {
-  res.send('Search results are here');
+  const userInput = _.get(req, 'query', {});
+  const context = {
+    resultsTotal: userInput.resultsTotal || 0,
+    query: userInput.query || 0
+  };
+  res.render('search-results', context);
 }
