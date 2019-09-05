169d3f1a4a0f82cf3fc39549dbe7ab026a1b5641
Vot <vot@siliconfen.co>
2019-09-05 10:06:08 +0100

    Add navbar

--- routes/home.js
+++ routes/home.js
@@ -1,12 +1,22 @@
-module.exports = (req, res, next) => {
-  const categories = [
-    'physiology',
-    'evolutionary-biology',
-    'microbiology',
-    'systems-biology',
-    'cell-biology',
-    'epidemiology'
-  ];
+const _ = require('lodash');
 
-  res.render('home', { categories });
+const postgres = require('../lib/postgres');
+
+module.exports = (req, res, next) => {
+
+  postgres.getFakeCategories((categoriesErr, categoriesData) => {
+    res.render('home', { categories: categoriesData });
+  });
+
+
+  // const categories = [
+  //   'physiology',
+  //   'evolutionary-biology',
+  //   'microbiology',
+  //   'systems-biology',
+  //   'cell-biology',
+  //   'epidemiology'
+  // ];
+
+  // res.render('home', { categories });
 }
--- routes/search.js
+++ routes/search.js
@@ -6,26 +6,30 @@ module.exports = (req, res, next) => {
   const category = _.get(req, 'params.category');
   const query = _.get(req, 'query.query');
 
-  return postgres.getFakeResults((queryErr, queryResults) => {
-    let results = queryResults;
+  return postgres.getFakeCategories((categoriesErr, categoriesData) => {
+    return postgres.getFakeResults((queryErr, queryResults) => {
+      let results = queryResults;
 
-    if (category) {
-      console.log('applying category', category);
-      results = _.filter(results, (item) => { return item.collection === category; });
-    }
+      if (category) {
+        console.log('applying category', category);
+        results = _.filter(results, (item) => { return item.collection === category; });
+      }
 
-    if (query) {
-      console.log('applying query', query);
-      results = _.filter(results, (item) => { return item.title.toLowerCase().indexOf(query.toLowerCase()) !== -1; });
-    }
+      if (query) {
+        console.log('applying query', query);
+        results = _.filter(results, (item) => { return item.title.toLowerCase().indexOf(query.toLowerCase()) !== -1; });
+      }
 
-    const context = {
-      resultsTotal: results.length || 0,
-      query: query,
-      category: category,
-      results: results
-    };
+      const context = {
+        resultsTotal: results.length || 0,
+        query: query,
+        category: category,
+        results: results,
+        categories: categoriesData
+      };
 
-    return res.render('search-results', context);
+      return res.render('search-results', context);
+    });
   });
+
 };
