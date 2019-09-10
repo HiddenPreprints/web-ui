86042292e673f4872be816819db3d677877a11ef
Vot <vot@siliconfen.co>
2019-09-10 22:48:15 +0100

    Move mappers logic to a separate lib

--- routes/middleware.js
+++ routes/middleware.js
@@ -1,13 +1,19 @@
+const _ = require('lodash');
+
 const api = require('../lib/api');
+const mappers = require('../lib/mappers');
 
 module.exports = (req, res, next) => {
+  const categoryKey = _.get(req, 'params.category');
+
   api.getCategories((categoriesErr, categoriesData) => {
     if (categoriesErr || !categoriesData) {
       const error = categoriesErr || new Error('No data returned from the service.');
       return next(error);
     }
 
-    res.locals.categories = categoriesData;
+    const categories = mappers.mapAllCategories(categoriesData, categoryKey);
+    res.locals.categories = categories;
     return next();
   });
 };
--- routes/search.js
+++ routes/search.js
@@ -1,91 +1,33 @@
 const _ = require('lodash');
 
 const api = require('../lib/api');
+const mappers = require('../lib/mappers');
 
-/**
- * Determines the class to be used on the article.
- * This controls the colour to be used in SI tag.
- *
- * @param {object} art Article as returned from the API
- * @return {string} Class to be applied in the UI
- */
-function determineUIClassForArticle(art) {
-  let uiClass = 'label-danger';
-  if (art.shadow_index < 31) {
-    uiClass = 'label-warning';
-  }
-  if (art.shadow_index < 16) {
-    uiClass = 'label-success';
-  }
-
-  return uiClass;
-}
-
-
-function mapCategoryToDisplayName(cat, allCategories) {
-  const catObject = _.find(allCategories, { key: cat });
-
-  if (catObject && catObject.name) {
-    return catObject.name;
-  }
-
-  return cat;
-}
-
-
-module.exports = (req, res, next) => {
-  const category = _.get(req, 'params.category');
+module.exports = (req, res) => {
+  const categoryKey = _.get(req, 'params.category');
   const query = _.get(req, 'query.query');
+  const categoriesData = res.locals.categories;
 
-  return api.getCategories((categoriesErr, categoriesData) => {
-  // return api.getFakeCategories((categoriesErr, categoriesData) => {
+  const category = _.find(categoriesData, { key: categoryKey });
 
-    return api.getArticles({ query, category }, (queryErr, queryResults) => {
-    // return api.getFakeArticles(query, (queryErr, queryResults) => {
-      let results = queryResults;
+  return api.getArticles({ query, category: categoryKey }, (queryErr, queryResults) => {
+    const results = queryResults;
 
-      const categories = _.map(categoriesData, (cat) => {
-        cat.active = false; // no idea why the active property persists but whatever
-
-        if (cat.key === category) {
-          cat.active = true;
-        }
-
-        return cat;
-      });
-
-      // if (!category) {
-      //   results = {
-      //     total: 0,
-      //     articles: []
-      //   };
-      // }
-
-      results.articles = _.map(results.articles, (art) => {
-        art.ui_class = determineUIClassForArticle(art);
-        art.category_display = mapCategoryToDisplayName(art.category, categoriesData);
-        return art;
-      });
-      // if (category) {
-      //   console.log('applying category', category);
-      //   results = _.filter(results, (item) => { return item.collection === category; });
-      // }
-
-      // if (query) {
-      //   console.log('applying query', query);
-      //   results = _.filter(results, (item) => { return item.title.toLowerCase().indexOf(query.toLowerCase()) !== -1; });
-      // }
-
-      const context = {
-        resultsTotal: results && results.total ? results.total : 0,
-        resultsDisplayed: results && results.articles ? results.articles.length : 0,
-        query,
-        category,
-        results: results && results.articles ? results.articles : [],
-        categories,
-      };
-
-      return res.render('search-results', context);
+    results.articles = _.map(results.articles, (art) => {
+      art.ui_class = mappers.getShadowIndexUIClass(art);
+      art.category_display = mappers.mapCategoryToDisplayName(art.category, categoriesData);
+      return art;
     });
+
+    const context = {
+      query,
+      category,
+      categories: categoriesData,
+      resultsTotal: results && results.total ? results.total : 0,
+      resultsDisplayed: results && results.articles ? results.articles.length : 0,
+      results: results && results.articles ? results.articles : [],
+    };
+
+    return res.render('search-results', context);
   });
 };
